3e15328c505d3e92a06f8a1b74a759ae13cc1b5a
jarocode <okolieuchenna890@gmail.com>
2024-06-19 13:09:42 +0100

    port config

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 
 async function bootstrap() {
-  
   const app = await NestFactory.create(AppModule);
-  const PORT = new ConfigService().get<number>("PORT");
-  const allowedOrigins: string[] = ['http://localhost:3000']; // Adjust as needed
+  const PORT = new ConfigService().getOrThrow<number>("PORT");
+  const allowedOrigins: string[] = ["http://localhost:3000"]; // Adjust as needed
   const corsOptions: CorsOptions = {
     origin: allowedOrigins,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     preflightContinue: false,
     optionsSuccessStatus: 204,
   };
